445cfae8c93ca4f8cc1970629416e065b1cce45e
ykerit <ykerforit@gmail.com>
2020-04-01 13:00:18 +0800

    delete old file

--- src/main/java/com/ykerit/Cluster/MasterNode/MasterNode.java
+++
@@ -1,8 +0,0 @@
-package com.ykerit.Cluster.MasterNode;
-
-import org.greatfree.cluster.root.container.ClusterServerContainer;
-
-public class MasterNode {
-    private ClusterServerContainer master;
-    private MasterNode() {}
-}
